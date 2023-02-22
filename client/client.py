3596d94f4a2fefaac0b6e7fc45e84ddf88cac455
lexuss05Od <93823742+Lex05ua@users.noreply.github.com>
2023-02-22 18:44:20 +0200

    Create client.py

---
+++ client/client.py
@@ -0,0 +1,24 @@
+# import socket library
+import socket
+
+# define the host and port of the server
+host = 'localhost'
+port = 8000
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to the server
+client.connect((host, port))
+
+while True:
+    # send a message to the server
+    message = input("Enter message: ")
+    client.sendall(message.encode())
+
+    # receive a response from the server
+    response = client.recv(1024).decode()
+    print(f"Received message from server: {response}")
+
+# close the socket
+sock.close()
